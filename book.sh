e4e8a944912f858843495f77aa992c138b9c611b
Blake Sharp <blakecsharp@gmail.com>
2023-02-02 11:24:05 -0500

    More shell updates'

---
+++ book.sh
@@ -0,0 +1,3 @@
+curl --location --request GET 'https://staging-api.resy.com/3/details?party_size=2&day=2020-06-23&config_id=rgs://resy/1042631/209590/2/2020-06-23/2020-06-23/16:00:00/2/Dining%20Room' \
+--header 'Authorization:  ResyAPI api_key="youarewhereyoueat"' \
+--header 'Content-Type: application/x-www-form-urlencoded'
